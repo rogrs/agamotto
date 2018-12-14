c63be9b1ad7cfae7195b77d7c907fe905c86d247
rogerio ribeiro <rogerio.rs@gmail.com>
2018-12-13 23:56:49 -0200

    Update files

--- src/main/java/br/com/rogrs/web/rest/errors/EmailAlreadyUsedException.java
+++
@@ -1,8 +0,0 @@
-package br.com.rogrs.web.rest.errors;
-
-public class EmailAlreadyUsedException extends BadRequestAlertException {
-
-    public EmailAlreadyUsedException() {
-        super(ErrorConstants.EMAIL_ALREADY_USED_TYPE, "Email address already in use", "userManagement", "emailexists");
-    }
-}
--- src/main/java/br/com/rogrs/web/rest/package-info.java
+++
@@ -1,4 +0,0 @@
-/**
- * Spring MVC REST controllers.
- */
-package br.com.rogrs.web.rest;
